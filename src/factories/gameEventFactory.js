363cfd938a7a45aabf3c39907f4d9ba53675c510
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2022-04-06 12:00:12 +0200

    game event factory refinement

--- src/factories/gameEventFactory.js
+++ src/factories/gameEventFactory.js
@@ -1,19 +1,27 @@
-const GameEvents = {
-    NewGame: 'new_game',
-    GamePoint: 'game_point',
-};
+module.exports = () => {
+    const GameEvents = {
+        NewGame: 'new_game',
+        GamePoint: 'game_point',
+    };
 
-const createNewGameEvent = ({ id, player1Id, player2Id, ts }) => ({
-    type: GameEvents.NewGame,
-    id,
-    player1Id,
-    player2Id,
-    ts
-});
+    const createNewGameEvent = ({ id, player1Id, player2Id, ts }) => ({
+        type: GameEvents.NewGame,
+        id,
+        player1Id,
+        player2Id,
+        ts
+    });
 
-const createGamePointEvent = ({ id, playerId, ts }) => ({
-    type: GameEvents.NewGame,
-    id,
-    playerId,
-    ts
-});
+    const createGamePointEvent = ({ id, playerId, ts }) => ({
+        type: GameEvents.GamePoint,
+        id,
+        playerId,
+        ts
+    });
+
+    return {
+        GameEvents,
+        createNewGameEvent,
+        createGamePointEvent
+    }
+}
