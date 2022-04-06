09faf5caff3b321dea02700932bf220d35b481b2
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2022-04-06 12:49:18 +0200

    storage logic

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,13 +1,20 @@
-module.exports = () => {
-    const getGameEvents = async () => {
+const { MongoClient } = require('mongodb');
 
+module.exports = async ({ config }) => {
+    const client = new MongoClient(config.url);
+    await client.connect();
+    const db = client.db(config.dbName);
+
+    const gameEvents = db.collection(config.collections.gameEvents);
+
+    await gameEvents.createIndex({ id: 1 });
+
+    const getGameEvents = async (id) => gameEvents.find({ id }).toArray();
+
+    const saveGameEvent = async (gameEvent) => gameEvents.insertOne(gameEvent);
+
+    return {
+        getGameEvents,
+        saveGameEvent,
     };
-
-    const saveNewGame = async () => {
-
-    };
-
-    const saveGamePoint = async () => {
-
-    };
-}
+};
