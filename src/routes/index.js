65fa0cb8c8674548207de1b2d97dc81f51e0107e
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2022-04-10 14:06:47 +0200

    Get /game return 404 if no newGame event stored

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,37 @@
+const { NotFoundError } = require('../errors');
+
 module.exports = ({ app, gameService }) => {
-    app.get('/game/:id', async (req, res) => {
-        const {params: { id }} = req;
-        const game = await gameService.getGame(Number(id));
-        res.send(game);
+    app.get('/game/:id', async (req, res, next) => {
+        try {
+            const {params: { id }} = req;
+            const game = await gameService.getGame(Number(id));
+            return res.send(game);
+        } catch (error) {
+            if (error instanceof NotFoundError) {
+                return res.status(404).send(error.message);
+            }
+            next(error);
+        }
     });
 
-    app.post('/game', async (req, res) => {
-        const { body } = req;
-        await gameService.storeGame(body);
-        res.send(body);
+    app.post('/game', async (req, res, next) => {
+        try {
+            const { body } = req;
+            await gameService.storeGame(body);
+            return res.send();
+        } catch (error) {
+            next(error);
+        }
     });
 
-    app.post('/game/point', async (req, res) => {
-        const { body } = req;
-        await gameService.storeGamePoint(body);
-        res.send();
+    app.post('/game/point', async (req, res, next) => {
+        try {
+            const { body } = req;
+            await gameService.storeGamePoint(body);
+            return res.send();
+        } catch (error) {
+            next(error);
+        }
     });
 };
 
